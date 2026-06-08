d8e3027179d9e7d57bcd935f49921b79c1fae13f
Alessandro Parenti <alessandroparenti@MacBook-Air-di-Alessandro.local>
2026-06-08 22:26:47 +0200

    fix dark mode

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="color-scheme" content="light only" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
